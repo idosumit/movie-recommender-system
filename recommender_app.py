e66bf620d8473032e44e4e84003852be755cc9f4
Sumit Pokharel <psumitcoding@gmail.com>
2023-04-15 21:42:16 +0900

    Update recommender_app.py

--- recommender_app.py
+++ recommender_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import requests
 
 def get_poster(movie_id):
-    response = requests.get("https://api.themoviedb.org/3/movie/{}?api_key=849f1680a50ddf85941e336db926b570".format(movie_id))
+    response = requests.get("https://api.themoviedb.org/3/movie/{}?api_key={api_key here}".format(movie_id))
     data = response.json()
     return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
 
